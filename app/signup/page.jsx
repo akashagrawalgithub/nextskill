2b83f030d48807ac862ba7a354cd874f8a507ee0
akashagrawalgithub <akash242018@gmail.com>
2023-11-17 11:00:14 +0530

    nextskill

---
+++ app/signup/page.jsx
@@ -0,0 +1,124 @@
+"use client";
+import Image from "next/image";
+import { useRouter } from "next/navigation";
+
+export default function Signup() {
+  const router = useRouter();
+
+  return (
+    <div className="flex justify-center mt-8">
+      <div style={{ minWidth: "30%" }}>
+        <div className="flex min-h-full shadow-lg flex-1 flex-col justify-center 
+px-6 py-12 lg:px-8 bg-white">
+          <div className="sm:mx-auto sm:w-full sm:max-w-sm">
+            <div className="flex justify-center">
+            <img src="https://cliply.co/wp-content/uploads/2020/09/442008570_ARTIST_AVATAR_3D_400px.gif" height={100} width={100} />
+            </div>
+            <h2 className="mt-1 text-center text-2xl font-bold leading-9 
+tracking-tight text-gray-900">
+              Create Account
+            </h2>
+          </div>
+
+          <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
+            <form className="space-y-6" action="#" method="POST">
+              <div>
+                <label
+                  htmlFor="text"
+                  className="block text-sm font-medium leading-6 text-gray-900"
+                >
+                  Name
+                </label>
+                <div className="mt-2">
+                  <input
+                    id="name"
+                    name="name"
+                    type="text"
+                    autoComplete="name"
+                    required
+                    className="block w-full rounded-md border-0 py-2 px-3
+text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 
+placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 
+sm:text-sm sm:leading-6"
+                  />
+                </div>
+              </div>
+
+              <div>
+                <div className="flex items-center justify-between">
+                  <label
+                    htmlFor="email"
+                    className="block text-sm font-medium leading-6 text-gray-900"
+                  >
+                    Email
+                  </label>
+                </div>
+                <div className="mt-2">
+                  <input
+                    id="email"
+                    name="email"
+                    type="email"
+                    autoComplete="current-email"
+                    required
+                    className="block w-full rounded-md border-0 py-2 px-3 text-gray-900 
+shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 
+focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                  />
+                </div>
+              </div>
+
+              <div>
+                <div className="flex items-center justify-between">
+                  <label
+                    htmlFor="confirmpassword"
+                    className="block text-sm font-medium leading-6 text-gray-900"
+                  >
+                    Password
+                  </label>
+                </div>
+                <div className="mt-2">
+                  <input
+                    id="password"
+                    name="password"
+                    type="password"
+                    autoComplete="current-password"
+                    required
+                    className="block w-full rounded-md border-0 py-2 px-3
+text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 
+placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 
+sm:text-sm sm:leading-6"
+                  />
+                </div>
+              </div>
+
+              <div>
+                <button
+                  type="submit"
+                  className="flex w-full justify-center rounded-md bg-indigo-600 
+px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm 
+hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 
+focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+                >
+                  Sign Up
+                </button>
+              </div>
+            </form>
+
+            <p className="mt-10 text-center text-sm text-gray-500">
+              Already have an account?{" "}
+              <span
+                onClick={() => {
+                  router.push("/");
+                }}
+                className="font-semibold leading-6 text-indigo-600 
+hover:text-indigo-500 cursor-pointer"
+              >
+                Login
+              </span>
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
